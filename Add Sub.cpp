23cc1fe43b63ff39ba2ba87f163d5dd57d2bede4
Ziad Emad <113251853+ziademadd@users.noreply.github.com>
2023-04-19 16:08:20 +0200

    Add files via upload

---
+++ Add Sub.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int A,B,C;
+    cout <<" Enter Number of A \n" ;
+    cin>>A;
+    cout <<" Enter Number of B \n" ;
+    cin>>B;
+
+    C = A + B ;
+
+    cout <<" A + B =  " << C << "\n";
+    cout << " Enter Number of A  \n" ;
+    cin>> A;
+    cout<< " Enter Number of B  \n";
+    cin>>B ;
+    C = A - B ;
+    cout << " A - B =   " << C ;
+
+    return 0;
+}
